1a2d4020bd5531f29d19a11827d9249602ed6a2c
Nirbhay <nirbhaypaliwal05@gmail.com>
2024-04-23 15:31:52 +0530

    New2

---
+++ mango/src/AssignProject.js
@@ -0,0 +1,92 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import Navbar from "./Navbar";
+
+const AssignProject = () => {
+  const [data, setData] = useState({});
+
+  const handleClick = () => {
+    // on submit
+
+  };
+
+  const handleInput = (e) => {
+    setData({ ...data, [e.target.id]: e.target.value }); // Send the data to the backend ....
+  };
+
+  return (
+    <div>
+      <Navbar />
+
+      <div className="mx-auto container  mt-8">
+        {" "}
+        {/*mx-auto */}
+        <h2 className="text-2xl font-bold mb-4">Assign Project</h2>
+        <div className="flex flex-col">
+          {/* Project Name */}
+          <div className="mb-4">
+            <label
+              htmlFor="projectName"
+              className="block text-sm font-bold mb-2"
+            >
+              Project Name:
+            </label>
+
+            <input
+              type="text"
+              id="projectName"
+              className="border border-gray-300 rounded py-2 px-3 w-full"
+              placeholder="Enter project name"
+              onChange={(e) => handleInput(e)}
+            />
+          </div>
+          {/* Deadline */}
+
+          <div className="mb-4">
+            <label htmlFor="deadline" className="block text-sm font-bold mb-2">
+              Deadline:
+            </label>
+            <input
+              type="date"
+              id="deadline"
+              className="border border-gray-300 rounded py-2 px-3 w-full"
+              onChange={(e) => handleInput(e)}
+            />
+          </div>
+
+          {/* Project Demands */}
+          <div className="mb-4">
+            <label
+              htmlFor="projectDemands"
+              className="block text-sm font-bold mb-2"
+            >
+              Project Demands:
+            </label>
+            <textarea
+              id="projectDemands"
+              className="border border-gray-300 rounded py-2 px-3 w-full"
+              rows="4"
+              placeholder="Enter project demands"
+              onChange={(e) => handleInput(e)}
+            ></textarea>
+          </div>
+        </div>
+        {/* Submit Button */}
+        
+          <div className="mt-10 flex justify-end">
+            <button
+              className="bg-teal-500 hover:bg-teal-600 text-white font-bold py-2 px-4 rounded   focus:outline-none focus:shadow-outline"
+              // onClick={handleClick}
+            >
+              Assign Project
+            </button>
+          </div>
+        
+
+
+      </div>
+    </div>
+  );
+};
+
+export default AssignProject;
